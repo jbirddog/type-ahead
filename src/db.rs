6da36bbd68ef35f3ee63e4dcb961da38f42166ec
Jon Herron <jon.herron@yahoo.com>
2023-03-23 16:06:59 -0400

    City search, just name returned

--- src/db.rs
+++ src/db.rs
@@ -6,6 +6,7 @@ pub type Pool = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
 
 pub enum Query {
     // TODO: String vs &str
+    CityNamesStartingWith(String, i32),
     CountryNamesStartingWith(String, i32),
     StateNamesStartingWith(String, i32),
 }
@@ -26,6 +27,10 @@ pub async fn execute(pool: &Pool, query: Query) -> Result<Vec<Data>, Error> {
         .map_err(error::ErrorInternalServerError)?;
 
     web::block(move || match query {
+        Query::CityNamesStartingWith(prefix, limit) => {
+            find_cities_starting_with(conn, prefix, limit)
+        }
+
         Query::CountryNamesStartingWith(prefix, limit) => {
             find_countries_starting_with(conn, prefix, limit)
         }
@@ -36,6 +41,22 @@ pub async fn execute(pool: &Pool, query: Query) -> Result<Vec<Data>, Error> {
     })
     .await?
     .map_err(error::ErrorInternalServerError)
+}
+
+// TODO: add country
+fn find_cities_starting_with(conn: Connection, prefix: String, limit: i32) -> QueryResult {
+    let mut stmt = conn.prepare(
+        "SELECT name 
+    FROM cities
+    WHERE name LIKE ? || '%'
+    ORDER BY name
+    LIMIT ?",
+    )?;
+
+    stmt.query_map(rusqlite::params![prefix, limit], |row| {
+        Ok(Data::Name(row.get(0)?))
+    })
+    .and_then(Iterator::collect)
 }
 
 // TODO: refactor countries/states_starting_with
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,17 @@ struct TypeAheadParams {
     limit: i32,
 }
 
+async fn find_cities_starting_with(
+    db: web::Data<Pool>,
+    query_params: web::Query<TypeAheadParams>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let query =
+        db::Query::CityNamesStartingWith(query_params.prefix.to_string(), query_params.limit);
+    let result = db::execute(&db, query).await?;
+
+    Ok(HttpResponse::Ok().json(result))
+}
+
 async fn find_countries_starting_with(
     db: web::Data<Pool>,
     query_params: web::Query<TypeAheadParams>,
@@ -52,6 +63,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(pool.clone()))
             // TODO: build the routes separately
             .service(web::resource("/").route(web::get().to(hello)))
+            .service(web::resource("/cities").route(web::get().to(find_cities_starting_with)))
             .service(web::resource("/countries").route(web::get().to(find_countries_starting_with)))
             .service(web::resource("/states").route(web::get().to(find_states_starting_with)))
     })
