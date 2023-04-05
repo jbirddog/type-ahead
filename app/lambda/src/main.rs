6f2e755dcf7fe5e89509086509a2d5e7209a906a
Jon Herron <jon.herron@yahoo.com>
2023-04-05 11:13:19 -0400

    Quick and dirty request routing. Need to chain the option/results better

--- app/lambda/src/main.rs
+++ app/lambda/src/main.rs
@@ -1,23 +1,48 @@
-use lambda_http::{run, service_fn, Body, Error, Request, Response}; //, RequestExt
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 use r2d2_sqlite::{self, SqliteConnectionManager};
+use serde_json::json;
 
 use type_ahead_db::{execute, Pool, Query};
+
+// TODO: will need a higher level dispatcher to handle liveness/discoverability endpoints
+fn query_for_request(event: Request) -> Option<Query> {
+    let prefix = event
+        .query_string_parameters()
+        .first("prefix")
+        .unwrap()
+        .to_string();
+    let limit: i32 = event
+        .query_string_parameters()
+        .first("limit")
+        .unwrap_or_else(|| "100")
+        .parse()
+        .unwrap();
+
+    match event.raw_http_path().as_str() {
+        "/v1/type-ahead/cities" => Some(Query::CityNamesStartingWith(prefix, limit)),
+        "/v1/type-ahead/states" => Some(Query::StateNamesStartingWith(prefix, limit)),
+        "/v1/type-ahead/countries" => Some(Query::CountryNamesStartingWith(prefix, limit)),
+        &_ => None,
+    }
+}
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
 
+    // TODO: look at the possibility of the connection pool being re-used?
+    // TODO: better handle errors vs the unwraps
     let manager = SqliteConnectionManager::file("data.db");
     let pool = Pool::new(manager).unwrap();
     let conn = pool.get().unwrap();
 
-    let query = Query::CityNamesStartingWith("marie".to_string(), 100);
+    let query = query_for_request(event).unwrap();
 
     let response = execute(conn, query).unwrap();
-    let response_str = serde_json::to_string(&response).unwrap();
+    let response_str = json!(response).to_string();
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
